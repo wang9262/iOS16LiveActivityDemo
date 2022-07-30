9d6a381f29b08508b96e75ff746ed1ca32bccddf
Vong <Vong9262@gmail.com>
2022-07-30 14:21:21 +0800

    Initial Commit

---
+++ LiveActivityDemo/LiveActivityDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivityDemoApp.swift
+//  LiveActivityDemo
+//
+//  Created by Vong on 2022/7/30.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivityDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
